5329a6cd65c177e12e5149eebdad94b8126c4b47
Mladen Stepanic <crawleyprint@gmail.com>
2019-12-28 12:30:09 +0100

    Display forms in as json dump

---
+++ index.js
@@ -0,0 +1,18 @@
+(function () {
+    const $ = query => document.querySelectorAll(query);
+    const form = $('form')[0];
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const isValid = form.checkValidity();
+        const validityObject = [].slice.call(form.elements).map(element => {
+            return Object.assign({}, { id: element.getAttribute('id') }, element.validity, { errorMsg: element.validationMessage });
+        });
+        if (!isValid) {
+            $('#error-dump')[0].innerHTML = JSON.stringify(validityObject, null, 2);
+            return false;
+        }
+
+        $('#error-dump')[0].innerHTML = 'All good!';
+        
+    })
+})();
