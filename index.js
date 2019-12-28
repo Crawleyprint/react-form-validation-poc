ac7f8eb17df2077ff41885fb275854d3a0108456
Mladen Stepanic <crawleyprint@gmail.com>
2019-12-28 12:44:47 +0100

    improved email validation

--- index.js
+++ index.js
@@ -5,8 +5,18 @@
         event.preventDefault();
         const isValid = form.checkValidity();
         const validityObject = [].slice.call(form.elements).map(element => {
-            return Object.assign({}, { id: element.getAttribute('id') }, element.validity, { errorMsg: element.validationMessage });
-        });
+            if (element.type === 'email' && element.validity.valid) {
+                if (!/\.[a-zA-Z]{2,}$/.test(element.value)) {
+                    element.setCustomValidity('Gimme a real email!')
+                }
+            }
+            return Object.assign(
+                {}, 
+                { id: element.getAttribute('id') }, 
+                element.validity, 
+                { errorMsg: element.validationMessage }
+        );
+    });
         if (!isValid) {
             $('#error-dump')[0].innerHTML = JSON.stringify(validityObject, null, 2);
             return false;
